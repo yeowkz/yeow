ef8d428a437186917bf88590f70e64cc1bdc1cd1
yeow <59030585+yeowkz@users.noreply.github.com>
2019-12-19 04:00:22 +0800

    ticketing system customer class

---
+++ Customer.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package themepark;
+
+/**
+ *
+ * @author User
+ */
+public class Customer {
+
+    protected String name;
+    protected int age;
+    protected String nationality;
+    protected String email;
+    protected String phoneNumber;
+    protected String ic;
+    protected String passportNum;
+
+    public Customer() {
+    }
+
+    public Customer(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassportNum() {
+        return passportNum;
+    }
+
+    public void setPassportNum(String passportNum) {
+        this.passportNum = passportNum;
+    }
+
+    public String getIC() {
+        return ic;
+    }
+
+    public void setIC(String ic) {
+        this.ic = ic;
+    }
+
+}
